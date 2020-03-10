64ecf59ca0ba9f6fb5c2b799020ea755adb79b68
Jesús Rubio Pérez <baguira@pop-os.localdomain>
2020-03-10 16:24:39 +0100

    Simpler example to avoid redundacy with the README

---
+++ examples/minimal.rs
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2019, Jesús Rubio <jesusprubio@gmail.com>
+ *
+ * This source code is licensed under the MIT License found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+use leg::*;
+
+fn main() {
+    info("Informational message", None, None);
+}
