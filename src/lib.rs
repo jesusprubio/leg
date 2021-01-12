f140bf7416918404205710a3b11c4f5e203e168a
Jesus Rubio <jesusprubio@gmail.com>
2021-01-13 00:24:08 +0100

    Drop license from files

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2019, Jesús Rubio <jesusprubio@gmail.com>
- *
- * This source code is licensed under the MIT License found in
- * the LICENSE.txt file in the root directory of this source tree.
- */
 use async_std::{eprint, eprintln, println};
 use colored::*;
 
